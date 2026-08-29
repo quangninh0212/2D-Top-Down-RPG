c405b148b6b2b33ea7df9c3a318d6a1af5c737c5
Quang Ninh <dangninh138@gmail.com>
2026-08-30 00:26:32 +0700

    Player Sprites + Animating The Player

--- Document/2d top down rpg part2/PlayerController.cs
+++ Document/2d top down rpg part2/PlayerController.cs
@@ -13,54 +13,44 @@ public class PlayerController : MonoBehaviour
     private SpriteRenderer mySpriteRender;
 
 
-    private void Awake()
-    {
+    private void Awake() {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRender = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         playerControls.Enable();
     }
 
-    private void Update()
-    {
+    private void Update() {
         PlayerInput();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         AdjustPlayerFacingDirection();
         Move();
     }
 
-    private void PlayerInput()
-    {
+    private void PlayerInput() {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
 
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move()
-    {
+    private void Move() {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
-    private void AdjustPlayerFacingDirection()
-    {
+    private void AdjustPlayerFacingDirection() {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x)
-        {
+        if (mousePos.x < playerScreenPoint.x) {
             mySpriteRender.flipX = true;
-        }
-        else
-        {
+        } else {
             mySpriteRender.flipX = false;
         }
     }
