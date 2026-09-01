16c9c4ac5db554ad2a3541b9d2445b3aaa27f07f
Quang Ninh <dangninh138@gmail.com>
2026-09-01 13:52:38 +0700

    Fade Transition

--- Document/2d top down rpg part17/Scripts/AreaExit.cs
+++ Document/2d top down rpg part17/Scripts/AreaExit.cs
@@ -10,19 +10,16 @@ public class AreaExit : MonoBehaviour
 
     private float waitToLoadTime = 1f;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.GetComponent<PlayerController>())
-        {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<PlayerController>()) {
             SceneManagement.Instance.SetTransitionName(sceneTransitionName);
             UIFade.Instance.FadeToBlack();
             StartCoroutine(LoadSceneRoutine());
         }
     }
 
-    private IEnumerator LoadSceneRoutine()
-    {
-        while (waitToLoadTime >= 0)
+    private IEnumerator LoadSceneRoutine() {
+        while (waitToLoadTime >= 0) 
         {
             waitToLoadTime -= Time.deltaTime;
             yield return null;
