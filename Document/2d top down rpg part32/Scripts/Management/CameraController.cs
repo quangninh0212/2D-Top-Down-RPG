05fc2d8d83e845f4ae9449f93a2eeca4e8e1ef2a
Quang Ninh <dangninh138@gmail.com>
2026-09-03 10:13:50 +0700

    Enemies + Ghost

--- Document/2d top down rpg part32/Scripts/Management/CameraController.cs
+++ Document/2d top down rpg part32/Scripts/Management/CameraController.cs
@@ -7,13 +7,11 @@ public class CameraController : Singleton<CameraController>
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
 
-    private void Start()
-    {
+    private void Start() {
         SetPlayerCameraFollow();
     }
 
-    public void SetPlayerCameraFollow()
-    {
+    public void SetPlayerCameraFollow() {
         cinemachineVirtualCamera = FindObjectOfType<CinemachineVirtualCamera>();
         cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
     }
